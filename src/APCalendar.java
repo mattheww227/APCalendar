0399b4409ebceca6909e8f176ebad488354899ec
MatthewWang <mattheww227@nycstudents.net>
2025-11-06 12:33:45 -0500

    first revision

--- src/APCalendar.java
+++ src/APCalendar.java
@@ -2,26 +2,42 @@ public class APCalendar
 {
     private static boolean isLeapYear(int year)
     {
-        return (year % 4) = 0;
+        boolean four = year % 4 == 0;
+        boolean hundred = year % 100 == 0;
+        boolean fourHundred = year % 400 == 0;
+        return four && (!hundred || fourHundred);
     }
 
     public static int numberOfLeapYears(int year1, int year2)
     {
-
+        int count = 0;
+        for (int i = year1; i <= year2; i++)
+        {
+            if (isLeapYear(i)) count++;
+        }
+        return count;
     }
 
     private static int firstDayOfYear(int year)
     {
-
+        int day = 1;
+        for (int i = 1; i < year; i++)
+        {
+            day++;
+            if (isLeapYear(i)) day++;
+        }
+        return day % 7;
     }
 
     private static int dayOfYear(int month, int day, int year)
     {
+        int days = 0;
 
+        return days;
     }
 
     public static int dayOfWeek(int month, int day, int year)
     {
-
+        return 0;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 public class Main
 {
-
+    public static void main(String[] args) {
+        System.out.println(APCalendar.numberOfLeapYears(1978, 2008));
+    }
 }
