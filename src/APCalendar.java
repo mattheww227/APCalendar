90b031805b77af1fb8a458dae1801ab201fa0b1b
mattheww227 <mattheww227@nycstudents.net>
2025-11-06 15:17:02 -0500

    Update APCalendar.java

--- src/APCalendar.java
+++ src/APCalendar.java
@@ -32,12 +32,25 @@ public class APCalendar
     private static int dayOfYear(int month, int day, int year)
     {
         int days = 0;
-
+        if (month > 1) days += 31;
+        if (month > 2) days += 28;
+        if (isLeapYear(year)) days++;
+        if (month > 3) days += 31;
+        if (month > 4) days += 30;
+        if (month > 5) days += 31;
+        if (month > 6) days += 30;
+        if (month > 7) days += 31;
+        if (month > 8) days += 31;
+        if (month > 9) days += 30;
+        if (month > 10) days += 31;
+        if (month > 11) days += 30;
+        days += day;
         return days;
     }
 
     public static int dayOfWeek(int month, int day, int year)
     {
-        return 0;
+        int days = dayOfYear(month, day, year) % 7;
+        return firstDayOfYear(year) + days - 1;
     }
 }
