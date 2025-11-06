f343a15e3d959fec3c8675a013cd075619dcf179
mattheww227 <mattheww227@nycstudents.net>
2025-11-06 15:16:24 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main
 {
     public static void main(String[] args) {
         System.out.println(APCalendar.numberOfLeapYears(1978, 2008));
+
+        System.out.println(APCalendar.dayOfWeek(1, 5, 2019));
+        System.out.println(APCalendar.dayOfWeek(1, 10, 2019));
     }
 }
